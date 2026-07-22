a27773a1b40719de164b34ec7d9bb0cdeccd870f
samay-topre <154588485+samay-topre@users.noreply.github.com>
2026-07-23 01:37:00 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-
 import Login from './src/screens/Login';
 import Home from './src/screens/Home';
 import ForgotID from './src/screens/ForgotID';
@@ -12,7 +11,7 @@ const Stack = createStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator screenOptions={{ headerShown: false, cardStyle: { backgroundColor: '#000' } }}>
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
         <Stack.Screen name="Login" component={Login} />
         <Stack.Screen name="Home" component={Home} />
         <Stack.Screen name="ForgotID" component={ForgotID} />
